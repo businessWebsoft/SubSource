e73d0e32f472cbc7652d1a90fae06dac8b758ce7
businessWebsoft <bwstesters2021@gmail.com>
2021-02-25 09:22:36 +0000

    Build 1

---
+++ test-output-thread/report.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "c6815023-35a0-4d16-8401-7f9fa9f5cb9f",
+    "feature": "To test the Shoppage functionality of Subsource",
+    "scenario": "Check Shop deatails",
+    "start": 1614244825816,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1614244863750,
+    "className": "passed"
+  },
+  {
+    "id": "de2164aa-93b7-4509-89de-e10a55122e2c",
+    "feature": "To test the Company functionality of Subsource",
+    "scenario": "Check Company deatails",
+    "start": 1614244825640,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1614244857912,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 13,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 14,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  }
+]);
+});
